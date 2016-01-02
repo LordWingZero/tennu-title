ad61cf2e45184e589318f99d6c3e02cdab2d5410
Victorio Berra <toryberra@gmail.com>
2016-01-02 00:35:53 +0000

    Removed old tennu-cooldown code

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 var title = require('./title');
 
 var TennuTitle = {
-    requiresRoles: ['admin', 'dbcore'],
+    requiresRoles: ['dbcore'],
     init: function(client, imports) {
 
         const helps = {
@@ -17,32 +17,9 @@ var TennuTitle = {
             throw Error("asay is missing some or all of its configuration.");
         }
 
-        var requiresAdminHelp = "Requires admin privileges.";
-
-        var isAdmin = imports.admin.isAdmin;
-        var adminCooldown = client._plugins.getRole("cooldown");
-        if (adminCooldown) {
-            var cooldown = titleConfig['cooldown'];
-            if (!cooldown) {
-                client._logger.warn('tennu-asay: Cooldown plugin found but no cooldown defined.')
-            }
-            else {
-                isAdmin = adminCooldown();
-                client._logger.notice('tennu-title: cooldowns enabled: ' + cooldown + ' seconds.');
-            }
-        }
-
         const dbATitlePromise = imports.dbcore.then(function(knex) {
             return title(knex);
         });
-
-        function adminFail(err) {
-            return {
-                intent: 'notice',
-                query: true,
-                message: err
-            };
-        }
 
         return {
             handlers: {
@@ -54,22 +31,10 @@ var TennuTitle = {
                     }
                 },
                 "!title": function(command) {
-                    return isAdmin(command.hostmask).then(function(isadmin) {
-
-                        // isAdmin will be "undefined" if cooldown system is enabled
-                        // isAdmin will be true/false if cooldown system is disabled
-                        if (typeof(isadmin) !== "undefined" && isadmin === false) {
-                            throw new Error(requiresAdminHelp);
-                        }
-
-                        // The call could take a second... or 5
-                        return dbATitlePromise.then(function(title) {
-                            return title.searchTitle(command.channel, client.nickname());
-                        })
-                        
-                    }).catch(adminFail);
+                    return dbATitlePromise.then(function(title) {
+                        return title.searchTitle(command.channel, client.nickname());
+                    })
                 }
-
             },
 
             help: {
